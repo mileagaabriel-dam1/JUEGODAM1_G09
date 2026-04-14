d301f443bce74ddcde80b09c3b742828dcc2300a
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-04-14 19:17:05 +0200

    Update main.java

--- src/main/main.java
+++ src/main/main.java
@@ -2,6 +2,6 @@ package main;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("🎮 Juego iniciado correctamente");
+        System.out.println(" Juego iniciado correctamente ");
     }
 }
