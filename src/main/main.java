aa45c6c49b71c3a5edd7c92b5fa0ad6356acaba8
mileagaabriel-dam1 <mileagaabriel@alumnes.ilerna.com>
2026-04-15 15:41:24 +0200

    FUNCIONAL EN CLASE

--- src/main/main.java
+++ src/main/main.java
@@ -2,7 +2,7 @@ package main;
 
 import Vista.VistaJavaFX;
 
-public class MAIN {
+public class main {
     public static void main(String[] args) {
         System.out.println("JOC DEL PINGÜ - DAM1 - G09");
         System.out.println("IAN RUBIO, OSCAR MUÑOZ, GABRIEL MILEA");
